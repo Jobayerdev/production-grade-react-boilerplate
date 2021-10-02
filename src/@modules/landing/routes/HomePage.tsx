2be8f61d4169f84f3dca6ffd30fad80232cfbeee
Jobayer Hossain <jobayerhossain977@gmail.com>
2021-10-02 19:28:16 +0600

    master: Added

--- src/@modules/landing/routes/HomePage.tsx
+++ src/@modules/landing/routes/HomePage.tsx
@@ -1,7 +1,12 @@
-import React from "react"
-
 const HomePage = () => {
-	return <div>HomePage HomePage</div>
+	return (
+		<div>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, saepe?
+			Eaque, modi. Dicta provident, mollitia dolorem in cumque at iusto optio
+			laboriosam perspiciatis facilis nam ex aliquid porro expedita
+			consequuntur?
+		</div>
+	)
 }
 
 export default HomePage
